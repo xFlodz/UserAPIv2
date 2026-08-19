535009e9c730a1a5ce8573559a7e3d51ed0a7e0c
xFlod <densss306@gmail.com>
2026-08-19 23:01:32 +0300

    change logging setup func

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,18 +1,26 @@
 import logging
+import sys
+
 from src.core.config  import Config
 
 class Logger:
     def __init__(self):
         self.level = Config.LOGGING_LEVEL
         self.datefmt = '%Y-%m-%d %H:%M:%S'
-        self.format = '[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)10s - %(message)s'
+        self.format = '[%(asctime)s.%(msecs)03d] %(module)s:%(lineno)d %(levelname)s - %(message)s'
 
     def setup(self):
-        logging.basicConfig(
-            level=self.level,
+        formatter = logging.Formatter(
+            fmt=self.format,
             datefmt=self.datefmt,
-            format=self.format,
         )
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        console_handler.setLevel(self.level)
+
+        logger = logging.getLogger(__name__)
+        logger.addHandler(console_handler)
 
 logger_config = Logger()
 
